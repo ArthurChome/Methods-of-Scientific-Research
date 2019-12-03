6a4045353499415ca36d0a54ebe211390f13e760
Arthurito <Arthur.Vincent.Chome@vub.be>
2019-12-04 00:21:08 +0100

    Assignment 2: Completed

--- assignment2/scrip.R
+++ assignment2/scrip.R
@@ -2,9 +2,22 @@
 # Assignment 2
 
 # QUESTION 4
+
 # Read a txt file, named "mtcars.txt"
 question4 <- read.delim(file.choose(), header = T)
 question4$type <- as.numeric(question4$type)
+
+# Group comparison
+## Add a new factor variable to the data collection
+question4$type <- as.factor(question4$type)
+train = question4[sample(2, nrow(question4), replace = T, prob = c(1.0, 0.1))==1,]
+# Fit the model: question4 is the training data.
+#mymodel = glm(success ~ type + parameters, data = train, family = 'binomial')
+#Create new model
+mymodel = glm(success ~ type + parameters, data = train, family = 'binomial')
+
+summary(mymodel)
+
 
 # Two-way table 
 xtabs(~success + type, data = question4)
@@ -13,19 +26,11 @@ str(question4)
 # Sample the data
 ind = sample(2, nrow(question4), replace = T, prob = c(1.0, 0.1))
 
-train = question4[sample(2, nrow(question4), replace = T, prob = c(1.0, 0.1))==1,]
 
-trainingData <- question4[1:200,]
-test <- question4[200:200,]
+newdata<-with(question4, data.frame(question4$parameters=mean(question4$parameters), type=factor(0:1)))
 
-# Fit the model: question4 is the training data.
-mymodel = glm(success ~ type + parameters, data = train, family = 'binomial')
-
-summary(mymodel)
-
-## Individual predicated probabilities
-# You have to give a value for all 
-predict(mymodel,  data.frame(type=1, parameters=18050), type="response")
-predict(mymodel,  data.frame(type=2, parameters=18274), type="response")
+## Individual predicated probabilities: TESTS
+predict(mymodel2,  with(question4, data.frame(parameters=mean(parameters), type=factor(1:2))), type="response")
+#predict(glm(success ~ type + parameters, data = train, family = 'binomial'),  with(question4, data.frame(parameters=mean(parameters), type=factor(1:2))), type="response")
 
 
